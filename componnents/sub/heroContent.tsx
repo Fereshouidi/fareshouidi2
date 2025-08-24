a02a4ab2059417ce1013079998ab8e443a7d06f5
fares <fereshouidi298@gmail.com>
2025-08-24 04:01:19 +0100

    fixing

--- componnents/sub/heroContent.tsx
+++ componnents/sub/heroContent.tsx
@@ -78,7 +78,7 @@ const HeroContent = () => {
             className='w-full h-full flex justify-center items-center text-white'
         >
             <img
-                src='/mainIconsdark.png'
+                src='/mainIconsdark.svg'
                 alt='work icons'
                 height={650}
                 width={650}
