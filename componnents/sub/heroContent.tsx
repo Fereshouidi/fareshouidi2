54ad030cea80cc33139b35daf35371685737f755
fares <fereshouidi298@gmail.com>
2025-08-24 03:55:26 +0100

    fixing

--- componnents/sub/heroContent.tsx
+++ componnents/sub/heroContent.tsx
@@ -78,7 +78,7 @@ const HeroContent = () => {
             className='w-full h-full flex justify-center items-center text-white'
         >
             <img
-                src='/mainIconsDark.svg'
+                src='/mainIconsdark.png'
                 alt='work icons'
                 height={650}
                 width={650}
